7f6afa54f8c68ae1f6197c9b9c8accbd19b2616a
Lucha <73822723+Luchaoao@users.noreply.github.com>
2024-02-25 11:52:18 +0800

    1st

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,6 +4,30 @@
 
 <template>
   <main>
-    
+    <div class="bg-blue-400 w-full h-screen">
+        <div class="content">
+            <div class="add">
+                <input type="text" id="" class="add-text" placeholder="請輸入內容">
+                <div class="add-Todo" type="button">新增</div>
+            </div>
+            <div class="complete-list">
+                <div class="all" data-search="all">全部</div>
+                <div class="complete" data-search="complete">已執行</div>
+                <div class="incomplete" data-search="incomplete">未執行</div>
+            </div>
+            <table class="todo">
+                <thead>
+                    <tr>
+                        <th>執行</th>
+                        <th>事項</th>
+                        <th>功能</th>
+                    </tr>
+                </thead>
+                <tbody class="data-show"></tbody>
+            </table>
+            <div class="enter-to-local" type="button">save local</div>
+        </div>
+    </div>
+
   </main>
 </template>
